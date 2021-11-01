379df610db464b43731b4eb0de7e5aa026ae1e41
nolanswaim <nolanswaim13@gmail.com>
2021-11-01 08:10:41 -0700

    todo final

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+const {Query} = require("pg/lib/native");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+router.get('/', async function(req, res, next) {
+  const {sequelize} = require("../models/index");
+  const {QueryTypes} = require("sequelize");
 
+  let toDoItems = await sequelize.query('select * from todo', {type: QueryTypes.SELECT})
+  res.render('index', {toDoItems});
+});
+router.get("/add", function (req, res){
+  res.render('create_todo');
+})
+router.post('/add', async function(req, res){
+  const {sequelize} = require("../models/index");
+  const{QueryTypes} = require("sequelize");
+  await sequelize.query('inster into todo(description) values (:description)', {
+    type: QueryTypes.INSERT,
+    replacement:{
+      description: req.body.description
+    }
+  });
+  res.redirect('/');
+});
 module.exports = router;
